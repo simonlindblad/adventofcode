2638bed12c5f8196c8c30a0d6f954e769946abfd
Simon Lindblad <simon.lindblad93@gmail.com>
2022-12-04 07:46:01 -0500

    Add solution to day 4

---
+++ src/day4.rs
@@ -0,0 +1,45 @@
+use std::fs::read_to_string;
+
+#[derive(Debug)]
+struct SectionRange {
+    start: i32,
+    end: i32,
+}
+
+impl SectionRange {
+    fn from_str(range: &str) -> Self {
+        let (start, end) = range.split_once('-').unwrap();
+        SectionRange {
+            start: start.parse().unwrap(),
+            end: end.parse().unwrap(),
+        }
+    }
+
+    fn contains(&self, other: &SectionRange) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    fn overlaps(&self, other: &SectionRange) -> bool {
+        !(self.start > other.end || self.end < other.start)
+    }
+}
+
+fn count_pairs(file: &str, predicate: fn(&(SectionRange, SectionRange)) -> bool) -> usize {
+    read_to_string(file)
+        .unwrap()
+        .lines()
+        .map(|l| {
+            let pair = l.split_once(',').unwrap();
+            (SectionRange::from_str(pair.0), SectionRange::from_str(pair.1))
+        })
+        .filter(predicate)
+        .count()
+}
+
+pub fn run(file: &str) {
+    println!(
+        "Part 1: {}",
+        count_pairs(file, |p| p.0.contains(&p.1) || p.1.contains(&p.0))
+    );
+    println!("Part 2: {}", count_pairs(file, |p| p.0.overlaps(&p.1)));
+}
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    adventofcode2022::day3::run(&args[1]);
+    adventofcode2022::day4::run(&args[1]);
 }
