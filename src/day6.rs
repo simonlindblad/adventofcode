f08a69863fa82b895897e6bbb751cd3d35b89ea3
Simon Lindblad <simon.lindblad93@gmail.com>
2022-12-06 06:28:38 -0500

    Add a solution to day 6

---
+++ src/day6.rs
@@ -0,0 +1,20 @@
+use std::collections::HashSet;
+use std::fs::read_to_string;
+
+fn find_marker(line: &str, count: usize) -> usize {
+    let window_start = line.as_bytes()
+        .windows(count)
+        .position(|w| HashSet::<&u8>::from_iter(w.iter()).len() == count)
+        .expect("No marker found");
+    window_start + count
+}
+
+pub fn run(file: &str) {
+    let content = read_to_string(file).unwrap();
+    // The actual input is only one line, but this makes validating
+    // the example input easier.
+    for line in content.lines() {
+        println!("Part 1: {}", find_marker(line, 4));
+        println!("Part 2: {}", find_marker(line, 14));
+    }
+}
